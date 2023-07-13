6b08e8ef8081796d2e96c8bd4ffda8875d00cd2c
DESKTOP-OH4MRFL\Louis Venhoff <Louis.venhoff@gmx.net>
2023-07-13 06:41:59 +0200

    Finished config reader

--- Backend/TrucksLogServer/index.js
+++ Backend/TrucksLogServer/index.js
@@ -1,16 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var configReader_1 = require("../classes/configReader");
+var configReader_1 = require("./classes/configReader");
 var fastify = require("fastify")({
     logger: false
 });
-var confReader = new configReader_1.default("test.json");
+var confReader = new configReader_1.default("Config.json", function (conf) { startServer(conf); });
 fastify.get("/", function (req, res) {
     res.send({ Status: "OK" });
 });
-fastify.listen({ port: 3000 }, function (err, addr) {
-    if (err) {
-        throw (err);
-    }
-    console.log("Listening on port: " + 3000);
-});
+var startServer = function (conf) {
+    fastify.listen({ port: conf.port }, function (err, addr) {
+        if (err) {
+            throw (err);
+        }
+        console.log("Listening on port: " + 3000);
+    });
+};
--- Backend/TrucksLogServer/index.ts
+++ Backend/TrucksLogServer/index.ts
@@ -1,23 +1,27 @@
-import ConfigReader from "../classes/configReader"
+import ConfigReader, { Config } from "./classes/configReader";
 
 const fastify = require("fastify")({
     logger:false
 })
 
-const confReader:ConfigReader = new ConfigReader("test.json");
+const confReader:ConfigReader = new ConfigReader("Config.json", (conf:Config) => {startServer(conf)});
 
 fastify.get("/", (req, res) => {
     res.send({Status:"OK"})
 })
 
 
+const startServer = (conf:Config) => {
+    fastify.listen({port: conf.port}, (err:any, addr:any) => {
+
+        if(err){
+            throw(err);
+        }
+        console.log("Listening on port: " + 3000);
+    
+    });
+}
 
 
-fastify.listen({port: 3000}, (err:any, addr:any) => {
 
-    if(err){
-        throw(err);
-    }
-    console.log("Listening on port: " + 3000);
 
-});
