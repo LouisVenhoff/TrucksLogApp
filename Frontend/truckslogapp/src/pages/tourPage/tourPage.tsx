b94eab003e740349b37d0c3f6e7b12c545348480
Louis.venhoff@gmx.net <Louis.venhoff@gmx.net>
2023-07-26 22:36:27 +0200

    Worked on TourPage Design

--- Frontend/truckslogapp/src/pages/tourPage/tourPage.tsx
+++ Frontend/truckslogapp/src/pages/tourPage/tourPage.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import "./tourPageStyle.css";
 import TourDisplay from "../../components/TourDisplay/tourDisplay";
 import { ArrowLeftIcon } from "@chakra-ui/icons";
 import TrucksLogLogo from "../../resources/TrucksLogLogo.png";
+
+import {motion, useMotionValueEvent, useScroll} from "framer-motion";
 
 type TourPageProps = {
   accountName: string;
 };
 
 const TourPage: React.FC<TourPageProps> = ({ accountName }) => {
-  return (
-    <div className="TourPageMainDiv">
+  
+    const elementRef = useRef(null);
+
+    const {scrollYProgress} = useScroll({container:elementRef});
+
+    const [scrollRate, setScrollRate] = useState<number>(1);
+
+    useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        setScrollRate(latest);
+      })
+      
+    const calculateAvatarSize = (scrollRate:number):number => 
+    {
+        return 1 - scrollRate;
+    }
+
+
+return (
+    <div ref={elementRef} className="TourPageMainDiv">
       {/* <div className="TourPageBackBtn">
         <ArrowLeftIcon boxSize={6} />
       </div> */}
       <div className="TourPageAvatarDiv">
-            <img src={TrucksLogLogo} />
+            <motion.img style={{scale: calculateAvatarSize(scrollRate)}}  src={TrucksLogLogo} />
       </div>
       <div className="TourPageWelcomeTextDiv">
         <h1>Willkommen, {accountName}</h1>
